b93862939fda9319123b487ae7f827f91e26c676
Rich Porter <rpwebdevelopment@gmail.com>
2024-10-23 12:54:54 +0100

    updated web routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,14 +15,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-})->middleware(['auth', 'verified'])->name('home');
+Route::middleware(['auth', 'verified'])
+    ->name('tasks.')
+    ->controller(TaskController::class)
+    ->group(function() {
+        Route::get('', 'index')->name('home');
+        Route::get('create', 'create')->name('create');
+        Route::post('create', 'store')->name('store');
+        Route::get('edit/{task}', 'edit')->name('edit');
+        Route::post('edit/{task}', 'update')->name('update');
+        Route::get('delete/{task}', 'destroy')->name('destroy');
+        Route::get('complete/{task}', 'complete')->name('complete');
+    });
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+Route::middleware('auth')
+    ->prefix('profile')
+    ->name('profile.')
+    ->controller(ProfileController::class)
+    ->group(function () {
+        Route::get('', 'edit')->name('edit');
+        Route::patch('', 'update')->name('update');
+        Route::delete('', 'destroy')->name('destroy');
+    });
 
 require __DIR__.'/auth.php';
